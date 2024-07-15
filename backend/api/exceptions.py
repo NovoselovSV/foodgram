4211c5ee21a287ff6ac4025a5c84cf3a520a6937
S.V. Novoselov <novoselovsv@bk.ru>
2024-07-15 17:48:22 +0300

    Delete InnerException

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -6,9 +6,3 @@ class ErrorException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Something in provided data wrong'
     default_code = 'errors'
-
-
-class InnerException(APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = 'Something on server side went wrong'
-    default_code = 'errors'
